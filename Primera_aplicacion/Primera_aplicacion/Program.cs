6ac25b3fcf993001d2474204cdf6b2b237225d0b
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-13 22:21:25 +0100

    Add files via upload

---
+++ Primera_aplicacion/Primera_aplicacion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primera_aplicacion
+{
+    class Program //biblioteca de clases conocida como API
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Empezando a programar con C#!");
+        }
+    }
+}
