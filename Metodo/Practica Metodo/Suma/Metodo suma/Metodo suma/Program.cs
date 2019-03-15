d936ac031d2c5843d56d9d09b445a851ef8262fa
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-15 13:48:30 -0600

    llevamos poco
    pero praticando 2 hora sin interrupcion y observando por que se hace esos pasos se aprende.

---
+++ Metodo/Practica Metodo/Suma/Metodo suma/Metodo suma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Metodo_suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sumaNumeros(7, 9);
+        }
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine("la suma de los dos numeros son :" + (num1 + num2));
+        }
+    }
+}
