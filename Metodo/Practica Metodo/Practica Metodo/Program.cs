73d2cf2dde9cbc2c1c10e92fdacf2162b15ebd57
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-15 20:35:41 +0100

    Add files via upload

---
+++ Metodo/Practica Metodo/Practica Metodo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practica_Metodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //un metodo nunca se va ejecutar si no lo llamas.
+            mensajeEnPantalla();
+            Console.WriteLine("enviando msj");
+        }
+
+       //aqui va buscar el metodo y lo ejecuta si exite.
+        static void mensajeEnPantalla()
+        {
+
+
+
+            Console.WriteLine("Mensaje desde pantalla ");
+        }
+    }
+    }
+
