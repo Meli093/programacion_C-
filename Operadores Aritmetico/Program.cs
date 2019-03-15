3eca5a0826907fcd4f74aab2eea05e18c19ed727
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-15 20:04:48 +0100

    Add files via upload
    constantes

---
+++ Operadores Aritmetico/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operadores_Aritmetico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416f;
+            Console.WriteLine("Introdce el valor medio de la radio");
+
+             double radio=  double.Parse(Console.ReadLine());
+
+            //float area = radio * radio * pi;//forma tipica
+            double area = Math.Pow(radio, 2)*pi;
+           // Console.WriteLine($"el area del circulo es:{area}");//solo se puede en c#
+            Console.WriteLine("el area del circulo es:" + area );//la clasica 
+        }
+    }
+}
