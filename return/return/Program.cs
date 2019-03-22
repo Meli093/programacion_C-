46865cbb5bcc7d3555d4019a90b32df27bc6b3a4
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-21 22:30:33 -0600

    rela

---
+++ return/return/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Program
+     {
+    static void Main(string[] args)
+    {
+        Console.WriteLine(sumaNumeros(7, 9));
+    }
+
+    static int sumaNumeros(int num1, int num2)
+
+    {
+        return num1 + num2;
+    }
+}
+
