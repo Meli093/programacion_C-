9627a3aff65b295e92d3ed7cd8f7a0d2c6fb45cb
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-25 10:12:09 -0600

    array
    este el comienso

---
+++ array/Array/Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []edades;
+            edades = new int[4];
+            //vamos almacenar nuestro array
+            edades[0] = 15;
+            edades[1] = 25;
+            edades[2] = 5;//si ejecutamos nos vamos a mostrar el valor 5
+            edades[3] = 19;
+
+            Console.WriteLine(edades[2]);
+        }
+    }
+}
