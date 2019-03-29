04a8274aa5137a5fd139fad3dab68b8c13d2acc3
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-28 21:53:25 -0600

    hay la llevamos

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numero = new Random();
+            int aletorio = numero.Next(0, 100);
+            int minumero;
+            int intentos = 0;
+            Console.WriteLine("introduce  un n° entre 0 y 100");
+
+            do
+            {
+                intentos++;
+                try
+                {
+                    minumero = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception ex)
+                {
+                  Console.WriteLine("has introducido un valor alto, los valores deven de ser 0 , 100");
+                    Console.WriteLine(ex.Message);
+                    minumero = 0;
+
+                }
+                    if (minumero > aletorio) Console.WriteLine("el es mas bajo");
+                    if (minumero < aletorio) Console.WriteLine("el es mas alto");
+                
+                } while (aletorio != minumero) ;
+                Console.WriteLine("continuara intentos"+intentos);
+
+            }
+            }
+        }
+    
+
