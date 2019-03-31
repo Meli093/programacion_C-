96e4e2c299638c1ca8592e2d20cbbd744f047df1
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-30 18:47:44 -0600

    Add files via upload
    hay vamos

---
+++ Excepciones 2/Excepciones/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numero = new Random();
+            int aletorio = numero.Next(0, 100);
+            int minumero;
+            int intentos = 0;
+            Console.WriteLine("introduce  un n° entre 0 y 100");
+
+            do
+            {
+                intentos++;
+                try
+                {
+                    minumero = int.Parse(Console.ReadLine());
+
+                }
+                /* catch (FormatException e)//primero se pone los especificos
+                 {
+                     Console.WriteLine("Has introducido  texto");
+                     minumero = 0;
+                 }*/
+
+                catch (Exception e) when (e.GetType() != typeof(FormatException))
+                {
+                    Console.WriteLine("ha habido error, los valores deven de ser 0");//los generales
+                    // Console.WriteLine(ex.Message);
+                    minumero = 0;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Has introducido  texto");
+                    minumero = 0;
+                }
+
+
+                    if (minumero > aletorio) Console.WriteLine("el es mas bajo");
+                    if (minumero < aletorio) Console.WriteLine("el es mas alto");
+                
+                } while (aletorio != minumero) ;
+                Console.WriteLine("continuara intentos"+intentos);
+
+            }
+            }
+        }
+    
+
---
+++ Excepciones 2/UsoChecked/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UsoChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                int numero = int.MaxValue;
+                int resultado = numero + 20;//va marcar un numero negativo
+
+                Console.WriteLine(resultado);
+            }
+        }
+    }
+}
