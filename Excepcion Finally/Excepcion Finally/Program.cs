e49c3ab9c82c5fe004819abd899377fd0387ecd1
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-04-02 12:23:53 -0600

    finally
    espero les sirva

---
+++ Excepcion Finally/Excepcion Finally/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Excepcion_Finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader archivo = null;
+            try
+            {
+                string linea;
+                int contador=0;
+                string path = @"C:\Users\MR\Desktop\tirar.txt";//si cambio el nombre del archivo
+                //nos marcaria error es cuando se activa la exception
+                archivo = new System.IO.StreamReader(path);
+                while ((linea = archivo.ReadLine())!= null)
+                {
+                    Console.WriteLine(linea);
+                     contador++;
+
+                }
+
+            }
+            catch(Exception e)
+
+            {
+                Console.WriteLine("error con la lectura del archivo");
+
+            }finally
+            {
+                if (archivo != null) archivo.Close();
+                Console.WriteLine("Conexion con el fichero cerrada");
+            }
+        }
+    }
+}
