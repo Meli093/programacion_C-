8e5d493acf9acecca6a2a95bf8fc583049815072
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-04-02 14:03:05 -0600

    funcionalidad

---
+++ switch/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eliga el medio de transporte (coche,tren,avion)");
+            string medioTranporte = Console.ReadLine();
+            switch (medioTranporte)
+            {
+                case "coche":
+                    Console.WriteLine("Velocidad media: 100 km/h");
+                    break;
+
+                case "tren":
+                    Console.WriteLine("Velocidad media: 250 km/h");
+                    break;
+
+                case "avion":
+                    Console.WriteLine("Velocidad media: 800 km/h");
+                    break;
+
+                default:
+                    Console.WriteLine("Transporte no contemplado");
+                    break;
+
+            }
+            Console.WriteLine("Ha finalizado el programa");
+        }
+
+    }
+}
